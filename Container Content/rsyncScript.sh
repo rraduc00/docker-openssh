7f69a64a4e354484756dff2df031fdac9fc8eece
Razvan Raducu <rrad@unileon.es>
2018-04-18 20:58:23 +0200

    Cron jobs added, as well as backup scripts

--- Container Content/rsyncScript.sh
+++ Container Content/rsyncScript.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 #./rsyncScript.sh lumn0 192.168.0.32:/home/lumn0/ttttt/asd
 user=$1
-ipypath=$2
+password=$2
+ipypath=$3
 
 array=(${ipypath//:/ })
 ruto=${array[1]}
 ip=${array[0]}
-echo  "contrasenia:"
-read pass
 
 expect<<END
-spawn ssh $user@$ip
+spawn ssh -oStrictHostKeyChecking=no $user@$ip
 expect "password: "
-send "$pass\r"
+send "$password\r"
 expect "$ "
 send "mkdir -p $ruto\r"
 expect "$ "
 send "exit\r"
 
 set timeout -1
-spawn rsync -av -r -e ssh /home/ $user@$ip:$ruto
+spawn rsync -av -r -e ssh -oStrictHostKeyChecking=no /home/ $user@$ip:$ruto
 expect "password:"
-send "$pass\r"
+send "$password\r"
 expect eof
--- Container Content/sftpScript.sh
+++ Container Content/sftpScript.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 #./sftpScript.sh lumn0 192.168.0.32:/home/lumn0/ttttt/asd
 user=$1
-ipypath=$2
+password=$2
+ipypath=$3
 
 array=(${ipypath//:/ })
 ruto=${array[1]}
 ip=${array[0]}
-echo  "contrasenia:"
-read pass
 
-expect<<END
-spawn ssh $user@$ip
+expect<<EOD
+spawn ssh -oStrictHostKeyChecking=no $user@$ip
 expect "password: "
-send "$pass\r"
+send "$password\r"
 expect "$ "
 send "mkdir -p $ruto\r"
 expect "$ "
 send "exit\r"
 spawn sftp $user@$ip
 expect "password:"
-send "$pass\n"
+send "$password\n"
 #expect "sftp>"
 #send "mkdir -p $ruto\n"
 expect "sftp>"
